32b856f75c2d6b5b10116609129ad9a55d8c8f16
minfrank35 <minfrank35@gmail.com>
2025-03-04 23:18:05 +0900

    HomeFragment -> RecycleGuideFragment action 추가

--- app/src/main/java/com/example/greenlens/view/fragment/HomeFragment.java
+++ app/src/main/java/com/example/greenlens/view/fragment/HomeFragment.java
@@ -4,49 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.fragment.app.Fragment;
 
-import com.example.greenlens.view.MainActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import com.example.greenlens.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-
-//        // 종이류 카드 클릭
-//        binding.cardPaper.setOnClickListener(v -> {
-//            ((MainActivity) requireActivity()).showRecycleGuide("paper");
-//        });
-//
-//        // 플라스틱 카드 클릭
-//        binding.cardPlastic.setOnClickListener(v -> {
-//            ((MainActivity) requireActivity()).showRecycleGuide("plastic");
-//        });
-//
-//        // 유리류 카드 클릭
-//        binding.cardGlass.setOnClickListener(v -> {
-//            ((MainActivity) requireActivity()).showRecycleGuide("glass");
-//        });
-//
-//        // 캔류 카드 클릭
-//        binding.cardCan.setOnClickListener(v -> {
-//            ((MainActivity) requireActivity()).showRecycleGuide("metal");
-//        });
-//
-//        // 비닐류 카드 클릭
-//        binding.cardVinyl.setOnClickListener(v -> {
-//            ((MainActivity) requireActivity()).showRecycleGuide("vinyl");
-//        });
-//
-//        // 스티로폼 카드 클릭
-//        binding.cardStyrofoam.setOnClickListener(v -> {
-//            ((MainActivity) requireActivity()).showRecycleGuide("styrofoam");
-//        });
-
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // NavController 가져오기
+        NavController navController = Navigation.findNavController(view);
+
+        // 클릭 리스너 설정
+        binding.cardPaper.setOnClickListener(v -> navigateToRecycleGuide(navController, "paper"));
+        binding.cardPlastic.setOnClickListener(v -> navigateToRecycleGuide(navController, "plastic"));
+        binding.cardGlass.setOnClickListener(v -> navigateToRecycleGuide(navController, "glass"));
+        binding.cardCan.setOnClickListener(v -> navigateToRecycleGuide(navController, "metal"));
+        binding.cardVinyl.setOnClickListener(v -> navigateToRecycleGuide(navController, "vinyl"));
+        binding.cardStyrofoam.setOnClickListener(v -> navigateToRecycleGuide(navController, "styrofoam"));
+    }
+
+    private void navigateToRecycleGuide(NavController navController, String type) {
+        HomeFragmentDirections.ActionHomeFragmentToRecycleGuideFragment action =
+                HomeFragmentDirections.actionHomeFragmentToRecycleGuideFragment(type);
+        navController.navigate(action);
     }
 
     @Override
